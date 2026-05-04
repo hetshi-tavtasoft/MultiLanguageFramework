4bc9851fdcdeb5b8c67d291e3ff3d4f721b235da
Hetshi Patel <pct113@server7.com>
2026-05-04 16:47:15 +0530

    Initial framework structure

---
+++ frameworks/ts-playwright/config/playwright.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `./config/.env.${process.env.ENV || 'qa'}` });
+
+export default defineConfig({
+  testDir: '../tests',
+  timeout: 30000,
+  retries: 2,
+  use: {
+    baseURL: process.env.BASE_URL,
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+});
