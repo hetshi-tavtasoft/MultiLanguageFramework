2567e632604f45c7f062979033b9c89f5edef91f
Hetshi Patel <pct113@server7.com>
2026-05-06 11:02:26 +0530

    Fixed Faker Issue for Ts

--- multiStructure/frameworks/ts-playwright/utils/dataFactory/userData.ts
+++ multiStructure/frameworks/ts-playwright/utils/dataFactory/userData.ts
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 
 export const userData = () => ({
-  firstname: faker.person.firstName(),
-  lastname: faker.person.lastName(),
-  zipcode: faker.location.zipCode()
+  firstname: faker.name.firstName(),
+  lastname: faker.name.lastName(),
+  zipcode: faker.address.zipCode()
 });
